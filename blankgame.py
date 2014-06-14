b0af1c8e3bebf76a8045f5f3af9a491cd5bb90e3
Ten Mutunhire <tmutunhire@gmail.com>
2014-06-14 01:01:10 -0400

    Add games in PyGame

---
+++ blankgame.py
@@ -0,0 +1,17 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello World!')
+while True: #main loops
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        pygame.display.update()
+        
+            
+        
+
+
---
+++ drawing.py
@@ -0,0 +1,10 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+#set up window
+DISPLAYSURFACE = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Drawing')
+
+#set up colors
