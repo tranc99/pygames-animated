f0af9fb7c125944107bff5857582ffeac22cc972
Ten Mutunhire <tmutunhire@gmail.com>
2014-06-14 02:58:20 -0400

    Add Binky Bill the game

---
+++ animation/catanimation.py
@@ -0,0 +1,58 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+FPS = 30
+fpsClock = pygame.time.Clock()
+
+#set up window
+DISPLAYSURFACE = pygame.display.set_mode((400, 300), 0, 32)
+pygame.display.set_caption('Animations! Blink Bill')
+
+WHITE = (255, 255, 255)
+catImg = pygame.image.load('cat.png')
+catx = 10
+caty = 10
+direction = 'right'
+
+while True:
+    DISPLAYSURFACE.fill(WHITE)
+
+    if direction == 'right':
+        catx += 5
+        if catx == 280:
+            direction = 'down'
+    elif direction == 'down':
+        caty += 5
+        if caty == 220:
+            direction = 'left'
+    elif direction == 'left':
+        catx -= 5
+        if catx == 10:
+            direction = 'up'
+    elif direction == 'up':
+        caty -= 5
+        if caty == 10:
+            direction = 'right'
+
+    DISPLAYSURFACE.blit(catImg, (catx, caty))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+
+    fpsClock.tick(FPS)
+
+    
+            
+            
+            
+    
+        
+
+
+
